b186a211bdd9eeb34b03a9b5db08f91f60e923e5
Chris Mear <chris@feedmechocolate.com>
2012-12-31 14:16:24 +0000

    Add new stages to full build script.

--- dependencies/scripts/build.sh
+++ dependencies/scripts/build.sh
@@ -3,5 +3,9 @@
 . $(dirname $0)/environment.sh
 
 try $(dirname $0)/build-python.sh
+try $(dirname $0)/build-sdl.sh
+try $(dirname $0)/build-libpng.sh
+try $(dirname $0)/build-pygame.sh
+try $(dirname $0)/build-renpy.sh
 
 echo 'Build done'
