8803df2d2868eff24ffd18aed92b3bbf933c7e87
Chris Mear <chris@feedmechocolate.com>
2012-12-31 14:15:34 +0000

    First pass of libpng build script.

---
+++ dependencies/scripts/build-libpng.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# ftp://ftp.simplesystems.org//pub/libpng/png/src/history/libpng12/libpng-1.2.49.tar.bz2
+
+. $(dirname $0)/environment.sh
+
+if [ ! -f $CACHEROOT/libpng-1.2.49.tar.bz2 ]; then
+  echo 'Downloading libpng source'
+  try curl ftp://ftp.simplesystems.org//pub/libpng/png/src/history/libpng12/libpng-1.2.49.tar.bz2 > $CACHEROOT/libpng-1.2.49.tar.bz2
+fi
+
+try rm -rf $TMPROOT/libpng-1.2.49
+echo 'Extracting libpng source'
+try tar xjf $CACHEROOT/libpng-1.2.49.tar.bz2
+try mv libpng-1.2.49 $TMPROOT
+
+pushd $TMPROOT/libpng-1.2.49
+
+echo 'Configuring libpng'
+try ./configure --prefix=$DESTROOT \
+  --host=arm-apple-darwin \
+  --enable-static=yes \
+  --enable-shared=no \
+  CC="$ARM_CC" AR="$ARM_AR" \
+  LDFLAGS="$ARM_LDFLAGS" CFLAGS="$ARM_CFLAGS"
+try make clean
+echo 'Building libpng'
+try make
+try make install
+
+echo 'Moving libpng build products into place'
+try cp $DESTROOT/lib/libpng12.a $BUILDROOT/lib
+
+try cp -a $DESTROOT/include/libpng12 $BUILDROOT/include
+
+try rm -f $BUILDROOT/include/png.h
+try ln -s libpng12/png.h $BUILDROOT/include/png.h
+
+try rm -f $BUILDROOT/include/pngconf.h
+try ln -s libpng12/pngconf.h $BUILDROOT/include/pngconf.h
