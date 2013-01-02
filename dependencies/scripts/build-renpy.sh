4800d49c5d75badeffcfd39bc6b2708211e086e4
Chris Mear <chris@feedmechocolate.com>
2013-01-02 00:54:56 +0000

    Get Ren'Py installing.

--- dependencies/scripts/build-renpy.sh
+++ dependencies/scripts/build-renpy.sh
@@ -9,7 +9,7 @@ if [ ! -f $CACHEROOT/renpy-$RENPY_VERSION-sdk.tar.bz2 ]; then
 fi
 
 # Clean any previous extractions,
-rm -rf $TMPROOT/renpy-$RENPY_VERSION-sdk
+rm -rdf $TMPROOT/renpy-$RENPY_VERSION-sdk
 # then extract Python source to cache directory
 echo 'Extracting RenPy SDK'
 try tar -xjf $CACHEROOT/renpy-$RENPY_VERSION-sdk.tar.bz2
@@ -36,6 +36,22 @@ HOSTPYTHON="$RENIOSDEPROOT/tmp/Python-$PYTHON_VERSION/hostpython"
 pushd module
 export RENIOS_IOS=1
 $HOSTPYTHON setup.py build_ext -g
+
+rm -rdf iosbuild
+$HOSTPYTHON setup.py install --root iosbuild
+
+bd=$TMPROOT/renpy-${RENPY_VERSION}-sdk/module/build/lib.macosx-*
+try $RENIOSDEPROOT/scripts/biglink $BUILDROOT/lib/librenpy.a $bd $bd/pysdlsound $bd/renpy/display $bd/renpy/gl $bd/renpy/text
+deduplicate $BUILDROOT/lib/librenpy.a
+
+# # Strip away the large stuff
+# # find iosbuild/ | grep -E '*\.(py|pyc|so\.o|so\.a|so\.libs)$$' | xargs rm
+
+try rm -rdf $BUILDROOT/renpy
+try mkdir -p $BUILDROOT/renpy
+try cp -a "$TMPROOT/renpy-${RENPY_VERSION}-sdk/common" "$BUILDROOT/renpy/"
+try cp -a "$TMPROOT/renpy-${RENPY_VERSION}-sdk/renpy" "$BUILDROOT/renpy/"
+
 popd
 
 export CC="$OLD_CC"
