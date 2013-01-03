92800a3e8b437c01e2dbabc2578fc200fd755db2
Chris Mear <chris@feedmechocolate.com>
2013-01-03 10:06:04 +0000

    Update build-all script.

--- dependencies/scripts/build.sh
+++ dependencies/scripts/build.sh
@@ -5,8 +5,12 @@
 try $(dirname $0)/build-python.sh
 try $(dirname $0)/build-sdl2.sh
 try $(dirname $0)/build-libpng.sh
-try $(dirname $0)/build-pygame.sh
+try $(dirname $0)/build-libjpeg-turbo.sh
+try $(dirname $0)/build-fribidi.sh
 try $(dirname $0)/build-freetype.sh
+try $(dirname $0)/build-sdl2_ttf.sh
+try $(dirname $0)/build-sdl2_image.sh
+try $(dirname $0)/build-pygame.sh
 try $(dirname $0)/build-renpy.sh
 
 echo 'Build done'
