0ca7e702a8aa001c8aac273137fd45439e757588
Chris Mear <chris@feedmechocolate.com>
2013-01-10 10:48:11 +0000

    Create template Xcode project based on empty Xcode template.

---
+++ examples/template/template/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  template
+//
+//  Created by Chris Mear on 10/01/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ examples/template/template/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  template
+//
+//  Created by Chris Mear on 10/01/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
