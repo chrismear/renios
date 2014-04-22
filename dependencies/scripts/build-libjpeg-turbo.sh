7dd3baa666013b20cc33e2d06008b82c03c36cfa
Chris Mear <chris@feedmechocolate.com>
2014-04-22 17:23:30 +0100

    Adjust libjpeg-turbo build script for Xcode 5 toolchain.

--- dependencies/scripts/build-libjpeg-turbo.sh
+++ dependencies/scripts/build-libjpeg-turbo.sh
@@ -2,6 +2,8 @@
 
 . $(dirname $0)/utils.sh
 
+export PATH="$PATH:`pwd`"
+
 if [ ! -f $CACHEROOT/libjpeg-turbo-1.2.0.tar.gz ]; then
   echo 'Downloading libjpeg-turbo source'
   try curl -L http://downloads.sourceforge.net/project/libjpeg-turbo/1.2.0/libjpeg-turbo-1.2.0.tar.gz > $CACHEROOT/libjpeg-turbo-1.2.0.tar.gz
@@ -16,12 +18,19 @@ pushd $TMPROOT/libjpeg-turbo-1.2.0
 
 echo 'Configuring libjpeg-turbo'
 try autoreconf -fiv
+
+# Under Xcode 5, this compile fails if -O is set to anything other than 0.
 try ./configure --prefix=$DESTROOT \
+  --with-jpeg8 \
   --host="$ARM_HOST" \
-  --enable-static=yes \
-  --enable-shared=no \
+  --enable-static \
+  --disable-shared \
   CC="$ARM_REAL_CC" AR="$ARM_AR" \
-  LDFLAGS="$ARM_LDFLAGS" CFLAGS="$ARM_CFLAGS"
+  LDFLAGS="-no-integrated-as $ARM_LDFLAGS" \
+  CFLAGS="-no-integrated-as $ARM_CFLAGS -O0" \
+  CPPFLAGS="$CFLAGS"
+  CCASFLAGS="-no-integrated-as $ARM_CFLAGS"
+
 try make clean
 echo 'Building libjpeg-turbo'
 try make
